178ac87fa042720004fbe499727502139a535f74
Nicoalz <nicobdx@icloud.com>
2021-09-17 12:14:20 +0200

    :hammer: add mulon student image

--- override-devinci.js
+++ override-devinci.js
@@ -1,1 +1,5 @@
 document.getElementsByTagName('body')[0].classList.add('override');
+
+var student_photo = document.getElementsByClassName("img-polaroid avatar")[0];
+
+student_photo.src = "https://zupimages.net/up/21/37/bjc1.jpg";
